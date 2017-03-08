0be3654acc3090e4d838635615d5a8b4f0799acc
Valdes Valdes <jesus.valdes.valdes@accenture.com>
2017-03-08 15:34:28 -0600

    style changes on pokemon list

--- app/scripts/controllers/pokemonListController.js
+++ app/scripts/controllers/pokemonListController.js
@@ -1,6 +1,10 @@
 (function(){
 	'use strict';
 
+	String.prototype.capitalize = function() {
+	    return this.charAt(0).toUpperCase() + this.slice(1);
+	}
+
 	angular
 		.module('pkApp.pokemonlist', [])
 		.controller('Pokemonlist', ['$scope', 'Pokemon', function ($scope, Pokemon){
@@ -8,9 +12,27 @@
 
 			console.log('Pokemonlist controller');
 
+			var pokemonList = [];
+
 			Pokemon.all().success(function(data){
-				console.log('success');
-				$scope.pokemons = data.results;
-			})
+				var results = data.results; 
+
+				console.log(results);
+				var pokemon = {};
+				var pokemonNum = 0;
+
+				for(var i = 0; i < results.length; i++){
+					
+					pokemon = {};
+					pokemonNum = i+1;
+					pokemon.name = results[i].name.capitalize();
+					pokemon.img = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + pokemonNum + '.png'
+					pokemon.number = pokemonNum;
+					pokemonList.push(pokemon);
+				}
+
+				$scope.pokemons = pokemonList;
+			});
+
 		}]);
 })();
--- app/scripts/factories/pokemon.js
+++ app/scripts/factories/pokemon.js
@@ -3,7 +3,10 @@
 		.factory('Pokemon', ['$http', function($http){
 			return {
 				all: function(){
-					return $http({method: 'GET', url: 'http://pokeapi.co/api/v2/pokemon/?limit=151'});
+					return $http.get('http://pokeapi.co/api/v2/pokemon/?limit=151', {cache: true});
+				},
+				byName: function(name){
+					return $http({method: 'GET', url: 'http://pokeapi.co/api/v2/pokemon/' + name});
 				}
 			};
 		}]);
